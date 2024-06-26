4002012bc8145917e963af567b42ca9d27d32aba
Fabio Masciarelli <158151713+FabioMasciarelli@users.noreply.github.com>
2024-06-26 09:46:54 +0200

    add: input type file in create.blade.php

--- resources/views/admin/create.blade.php
+++ resources/views/admin/create.blade.php
@@ -5,6 +5,11 @@
     <form action="{{ route('admin.projects.store') }}" method="POST" enctype="multipart/form-data">
         @csrf
 
+        <div class="input-group mb-3 mt-5">
+            <input type="file" class="form-control" id="uploadfile">
+            <label class="input-group-text" for="uploadfile" name="uploadfile">Upload file</label>
+        </div>
+
         <div class="mb-3">
             <label for="title" class="form-label">Title</label>
             <input type="text" class="form-control" id="title" name="title">
@@ -27,14 +32,9 @@
 
         <div class="mb-3">
             <label for="latest_fix" class="form-label">Latest fix</label>
-            <input type="text" class="form-control" id="latest_fix" name="latest_fix">
+            <input type="text" class="form-control" id="latest_fix" name="latest_fix" placeholder="YYYY-MM-DD">
         </div>
 
-        <div class="input-group mb-3">
-            <input type="file" class="form-control" id="uploadfile">
-            <label class="input-group-text" for="uploadfile" name="uploadfile">Upload</label>
-        </div>
-
-        <button type="submit" class="btn btn-success">Crea</button>
+        <button type="submit" class="btn btn-success">Create</button>
     </form>
 @endsection
